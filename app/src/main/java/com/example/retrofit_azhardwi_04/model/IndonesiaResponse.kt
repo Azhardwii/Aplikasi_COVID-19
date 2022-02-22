c7083a18da95f74864d6f7abd62769fcd9a5cc31
Azhardwii <halloazhardwii@gmail.com>
2022-02-22 10:51:42 +0700

    Aplikasi COVID-19

---
+++ app/src/main/java/com/example/retrofit_azhardwi_04/model/IndonesiaResponse.kt
@@ -0,0 +1,9 @@
+package com.example.retrofit_azhardwi_04.model
+
+data class IndonesiaResponse(
+    val name: String,
+    val positif: String,
+    val sembuh: String,
+    val meninggal: String,
+    val dirawat: String
+)
---
+++ app/src/main/java/com/example/retrofit_azhardwi_04/model/ProvinceResponse.kt
@@ -0,0 +1,4 @@
+package com.example.retrofit_azhardwi_04.model
+data class ProvinceResponse(
+    val attributes: Province
+)
